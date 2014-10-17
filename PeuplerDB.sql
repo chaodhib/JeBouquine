84bb3b04fd83d542665b4af9661a41ddbfcbe5f7
Chaouki <chaodhib@gmail.com>
2014-10-17 16:54:37 +0200

    y

--- PeuplerDB.sql
+++ PeuplerDB.sql
@@ -1,13 +1,13 @@
-INSERT INTO jebouquinedb.category (NAME)
+INSERT INTO jebouquine.CATEGORY (NAME)
 VALUES ('Informatique'), ('Dessin'), ('Jardinage');
-INSERT INTO jebouquinedb.language (NAME)
+INSERT INTO jebouquine.LANGUAGE (NAME)
 VALUES ('fr'), ('en'), ('ndls');
-INSERT INTO jebouquinedb.publisher (NAME)
+INSERT INTO jebouquine.PUBLISHER (NAME)
 VALUES ('Manning'), ('Del Rey'), ('Atlas'), ('Hachette'), ('Gallimard');
-INSERT INTO jebouquinedb.author (FIRSTNAME, LASTNAME)
+INSERT INTO jebouquine.AUTHOR (FIRSTNAME, LASTNAME)
 VALUES ('Chaouki', 'Dhib'), ('Claude', 'Francois'), ('Abra', 'Cadabra'), ('Jean', 'Valjean'), ('Gaston', 'Lagaffe');
 
-INSERT INTO jebouquinedb.book (TITLE, YEAR, PRICE, ISBN, AVAILABLE, NOVELTY, BESTSALES, author_ID, category_ID, publisher_ID, IMAGE)
+INSERT INTO jebouquine.BOOK (TITLE, YEAR, PRICE, ISBN, AVAILABLE, NOVELTY, BESTSALES, author_ID, category_ID, publisher_ID, IMAGE)
 VALUES ('Un long dimanche de fiancailles', 2014, 20.99, '1786', TRUE, TRUE, FALSE, 5, 2, 2, 'https://i.imgur.com/kmvxYCQ.jpg'),
 ('La huitième couleur', 1997, 12.99, '1477', TRUE, FALSE, TRUE, 1, 1, 3,'https://i.imgur.com/zIRZ4k8.jpg' ),
 ('Des souris et des hommes', 1989, 11.49, '2473', TRUE, FALSE, TRUE, 3, 3, 1, 'https://i.imgur.com/schujsE.jpg'),
@@ -17,14 +17,13 @@ VALUES ('Un long dimanche de fiancailles', 2014, 20.99, '1786', TRUE, TRUE, FALS
 ('Struts pour les nuls', 2003, 32.29, '3889', TRUE, FALSE, TRUE, 1, 1, 5, 'https://imgur.com/Ky0kXJs.jpg' ),
 ('La mer du nord', 1992, 9.99, '8925', TRUE, FALSE, FALSE, 4, 3, 5, 'https://imgur.com/g4frZtG.jpg');
 
-INSERT INTO jebouquinedb.client (LOGIN, PASSWORD, EMAIL, FIRSTNAME, LASTNAME)
-VALUES('Annie', 'password', 'anne.onyme@gmail.com', 'Anne', 'Onyme'),
-('GodZilla', 'dickbutt', 'camilleLeonard@gmail.com', 'Camille', 'Leonard');
+INSERT INTO jebouquine.CLIENT (LOGIN, PASSWORD, EMAIL, FIRSTNAME, LASTNAME)
+VALUES('Annie', 'password', 'anne.onyme@gmail.com', 'Anne', 'Onyme');
 
-INSERT INTO jebouquinedb.orderstate(NAME)
+INSERT INTO jebouquine.ORDERSTATE(NAME)
 VALUES('En attente'),('Preparation'),('Livraison'),('Recue');
 
-INSERT INTO jebouquinedb.comment (CONTENT, book_ID, client_ID)
-VALUES ('(with an indian accent) veryy good book, verryy good!', 1, 1)
+INSERT INTO jebouquine.COMMENT (CONTENT, book_ID, client_ID)
+VALUES ('Great book! 10/10', 1, 1)
 
 
